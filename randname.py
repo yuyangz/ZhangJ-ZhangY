fa01c383662d75aa0dda7fc356f36517b0d88485
Yuyang Zhang <yzhang4@stuy.edu>
2017-09-13 13:12:51 -0400

    Added python file

---
+++ randname.py
@@ -0,0 +1,29 @@
+Jennifer Zhang & Yuyang Zhang
+SoftDev1 pd07
+HW02 -- NO-body expects the Spanish Inquistion!
+2017-09-13
+
+
+
+
+import random
+
+
+CLASSES =  {
+   4: [ 'Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson', 'Brian', 'Farhan', 'Janet', 'Harry', 'Kevin', 'Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth', 'Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhiraj', 'Amy', 'Arvind', 'Nobel', 'Angela', 'Edward', 'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina'],
+   8: ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki', 'Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda', 'Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vncent', 'Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel', 'Felix', 'Jacob', 'Bayle', 'Fortune', 'Gio', 'Kelly', 'William', 'Jordan', 'Haley', 'Henry'],
+   9: ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C', 'Joel', 'Steph', 'Xinhui', 'Richard', 'Misha', 'Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry', 'Michael', 'Stiven', 'Will',  'Olivia', 'Constantine', 'Jessica', 'Kate', 'Shamaul', 'Max', 'Sarah', 'Anthony L', 'Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']
+}
+
+def randname(p):
+    print(random.choice(CLASSES[p]))
+
+
+print("Period 4")
+randname(4)
+print("")
+print("Period 8")
+randname(8)
+print("")
+print("Period 9")
+randname(9)
